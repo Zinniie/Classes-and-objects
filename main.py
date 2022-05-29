325b57c58d06b477c613ddc27221e6d2ae77955a
Zinniie <105227382+Zinniie@users.noreply.github.com>
2022-05-29 15:48:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 
-class Student():
+class Bob():
     # [assignment] Skeleton class. Add your code here
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        print ("The student new name is ", name, "and his age is", age)
+        print ("name= ", name)
+        print ("age=", age)
     def addme (self, tracks):
         self.tracks = tracks
-        print ("His new track is ", tracks)
-    def student_score(self, score):
+        print ("track= ", tracks)
+    def Bob_score(self, score):
         self.score = 80
-        print("The student score is", "80")
+        print("score=", "80")
        
 
 
 
 #calling a class
-s = Student("Sia",74)
-print (s.addme("UI"))
-print (s.student_score(80))
+Bob = Bob("Sia",74)
+print (Bob.addme("UI"))
+print (Bob.Bob_score(80))
 
 
 
