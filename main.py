f72fa733556f59e4cba37aa2df528d4381e6c6f3
Zinniie <105227382+Zinniie@users.noreply.github.com>
2022-05-30 07:47:24 +0100

    Adjustments so as to adhere to task instructions

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
-
-class Bob():
+#created class as student
+class Student:
     # [assignment] Skeleton class. Add your code here
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        print ("name= ", name)
-        print ("age=", age)
-    def addme (self, tracks):
-        self.tracks = tracks
-        print ("track= ", tracks)
-    def Bob_score(self, score):
-        self.score = 80
-        print("score=", "80")
-       
+    def __init__(self, name, age, tracks, score):
+          self.name = str (name)
+          self.age = int (age)
+          self.tracks = list (tracks)
+          self.score= float (score)
 
+          print("Student Information:")
+    
+    # A function to change the name of the student
+    def change_name (self, new_name):
+          self.name= new_name
+          print("name", self.name)
 
+    # A function to change the age of the student
+    def change_age(self, new_age):
+          self.age = new_age
+          print("age", self.age)
 
-#calling a class
-Bob = Bob("Sia",74)
-print (Bob.addme("UI"))
-print (Bob.Bob_score(80))
+    # A function to add track to existing list of tracks
+    def add_track(self, added_track):
+          self.tracks.append(added_track)
+          print("tracks", self.tracks)
+
+    # A function to get the student's score
+    def get_score (self):
+        return(self.score)
+
+#creating an Instance of the class together with the properties
+Bob = Student (name="Bob",age= 26, tracks=["FE","BE"],score= 20.90)
+
+# Expected methods
+print(Bob.change_name ("Sia") )
+print (Bob.change_age (74) )
+print(Bob.add_track("JAVA"))
+print(Bob.get_score())
 
 
 
